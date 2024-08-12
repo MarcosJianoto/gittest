c2a02da65567b6a310a8109d8f020740c959f548
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-08-12 07:40:24 -0300

    produtos1

---
+++ Agosto_exercicios/JavaNormal/12-08-2024_exercicios/src/App.java
@@ -0,0 +1,158 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entities.Produtos;
+
+public class App {
+
+    public static ArrayList<Produtos> list = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        produtos();
+    }
+
+    public static void produtos() {
+        boolean rodando = true;
+
+        while (rodando) {
+            try {
+                System.out.println("O que gostaria de fazer?");
+                System.out.println("[1] Adicionar produto");
+                System.out.println("[2] Atualizar produto");
+                System.out.println("[3] Listar todos os produtos");
+                System.out.println("[4] Remover produto");
+                System.out.println("[5] SAIR");
+                System.out.print("Escolha sua opção: ");
+                String opcao = sc.nextLine();
+
+                switch (opcao) {
+                    case "1": {
+                        System.out.println("===== ADICIONAR PRODUTO =====");
+                        System.out.print("Código do produto: ");
+                        Long id = sc.nextLong();
+                        sc.nextLine();
+                        System.out.print("Nome do produto: ");
+                        String name = sc.nextLine();
+                        System.out.print("Preço do produto: ");
+                        Double price = sc.nextDouble();
+                        System.out.print("Quantidade do produto: ");
+                        Integer quantity = sc.nextInt();
+                        sc.nextLine();
+
+                        Produtos produtos = new Produtos(id, name, price, quantity);
+                        list.add(produtos);
+                        System.out.println("---> Produto adicionado com sucesso! ");
+                        System.out.println();
+
+                        break;
+                    }
+                    case "2": {
+                        System.out.println("==== ATUALIZAR PRODUTO ==== ");
+
+                        if (list.isEmpty()) {
+                            System.out.println("---> Lista de produtos vazia!");
+                            System.out.println();
+                        } else {
+                            System.out.println("O que você gostaria de atualizar no produto? ");
+                            System.out.println("[1] - ID]");
+                            System.out.println("[2] - Name] ");
+                            System.out.println("[3] - Price]");
+                            System.out.println("[4]- Quantity]");
+                            System.out.print("Escolha sua opção: ");
+                            String opcaoAtualizarProduto = sc.nextLine();
+
+                            switch (opcaoAtualizarProduto) {
+                                case "1": {
+                                    for (int i = 0; i < list.size(); i++) {
+                                        System.out.println("\t[" + i + "] " + list.get(i).getName());
+                                    }
+
+                                    System.out.println();
+                                    System.out.print("Qual produto gostaria de alterar?");
+                                    int qualProdutoAlterar = sc.nextInt();
+                                    sc.nextLine();
+
+                                    al
+
+                                    break;
+                                }
+                                case "2": {
+                                    for (int i = 0; i < list.size(); i++) {
+                                        System.out.println("\t[" + i + "] " + list.get(i).getName());
+                                    }
+
+                                    System.out.println();
+                                    System.out.print("Qual produto gostaria de alterar?");
+                                    int qualProdutoAlterar = sc.nextInt();
+                                    sc.nextLine();
+
+                                    for (int i = 0; i < list.size(); i++) {
+                                        if (qualProdutoAlterar == i) {
+                                            System.out.print("Digite o novo Nome: ");
+                                            String name = sc.nextLine();
+                                            list.get(i).setName(name);
+                                            System.out.println("----> Nome do produto alterado");
+                                        }
+
+                                    }
+
+                                }
+
+                                default:
+                                    break;
+                            }
+
+                        }
+
+                        break;
+                    }
+
+                    case "3": {
+                        System.out.println("==== LISTAR TODOS OS PRODUTOS ====");
+                        if (list.isEmpty()) {
+                            System.out.println("---> Lista está vazia");
+                            System.out.println();
+                        } else {
+                            for (Produtos produto : list) {
+                                System.out.println(produto);
+                            }
+                        }
+
+                        break;
+                    }
+                    default: {
+                        System.out.println("Essa opção não existe, escolha novamente");
+                        break;
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+        }
+
+    }
+
+    public static void alterarProduto(int qualProdutoAlterar) {
+
+        try {
+            for (int i = 0; i < list.size(); i++) {
+                if (qualProdutoAlterar == i) {
+                    System.out.print("Digite o novo ID: ");
+                    Long id = sc.nextLong();
+                    list.get(i).setId(id);
+                    System.out.println("----> ID do produto alterado");
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ Agosto_exercicios/JavaNormal/12-08-2024_exercicios/src/entities/Produtos.java
@@ -0,0 +1,64 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Produtos {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Produtos() {
+    }
+
+    public Produtos(Long id, String name, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                ", price='" + getPrice() + "'" +
+                ", quantity='" + getQuantity();
+
+    }
+
+}
